7230fb41551f7c34f53b50044af0982d23b6dc26
Mathieu Fontenille <fontenille.mathieu@gmail.com>
2016-01-20 11:39:22 +0100

    Fichier base de données propre

---
+++ Documents du projet/base.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Users
+(
+ID_Users INT NOT NULL AUTO_INCREMENT,
+Name TEXT NOT NULL,
+Pass TEXT NOT NULL,
+PRIMARY KEY (ID_Users)
+);
+
+CREATE TABLE Messagerie
+(
+ID_Messagerie INT NOT NULL AUTO_INCREMENT,
+fk_ID_Users INT NOT NULL,
+fk_ID_Conversation INT,
+PRIMARY KEY (ID_Messagerie),
+FOREIGN KEY (fk_ID_Users) REFERENCES Users(ID_Users),
+FOREIGN KEY (fk_ID_Conversation) REFERENCES Conversation(ID_Conversation)
+);
+
+
+CREATE TABLE Message
+(
+ID_Message INT NOT NULL AUTO_INCREMENT,
+DateMessage datetime DEFAULT NOW(),
+fk_ID_Users INT NOT NULL,
+Text TEXT NOT NULL,
+PRIMARY KEY (ID_Message),
+FOREIGN KEY (fk_ID_Users) REFERENCES Users(ID_Users)
+);
+
+
+CREATE TABLE Conversation
+(
+ID_Conversation INT NOT NULL AUTO_INCREMENT,
+Titre TEXT NOT NULL,
+fk_ID_Users INT,
+PRIMARY KEY (ID_Conversation),
+FOREIGN KEY (fk_ID_Users) REFERENCES Users(ID_Users)
+);
---
+++ Documents du projet/projet_transversal.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 20 Janvier 2016 à 11:37
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `projet_transversal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conversation`
+--
+
+CREATE TABLE IF NOT EXISTS `conversation` (
+  `ID_Conversation` int(11) NOT NULL AUTO_INCREMENT,
+  `Titre` text NOT NULL,
+  `fk_ID_Users` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Conversation`),
+  KEY `fk_ID_Users` (`fk_ID_Users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `ID_Message` int(11) NOT NULL AUTO_INCREMENT,
+  `DateMessage` datetime DEFAULT CURRENT_TIMESTAMP,
+  `fk_ID_Users` int(11) NOT NULL,
+  `Text` text NOT NULL,
+  PRIMARY KEY (`ID_Message`),
+  KEY `fk_ID_Users` (`fk_ID_Users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messagerie`
+--
+
+CREATE TABLE IF NOT EXISTS `messagerie` (
+  `ID_Messagerie` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_ID_Users` int(11) NOT NULL,
+  `fk_ID_Conversation` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_Messagerie`),
+  KEY `fk_ID_Users` (`fk_ID_Users`),
+  KEY `fk_ID_Conversation` (`fk_ID_Conversation`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID_Users` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` text NOT NULL,
+  `Pass` text NOT NULL,
+  PRIMARY KEY (`ID_Users`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD CONSTRAINT `conversation_ibfk_1` FOREIGN KEY (`fk_ID_Users`) REFERENCES `users` (`ID_Users`);
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`fk_ID_Users`) REFERENCES `users` (`ID_Users`);
+
+--
+-- Contraintes pour la table `messagerie`
+--
+ALTER TABLE `messagerie`
+  ADD CONSTRAINT `messagerie_ibfk_1` FOREIGN KEY (`fk_ID_Users`) REFERENCES `users` (`ID_Users`),
+  ADD CONSTRAINT `messagerie_ibfk_2` FOREIGN KEY (`fk_ID_Conversation`) REFERENCES `conversation` (`ID_Conversation`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
